03ede2ae7f75b931c07b4b75b92e44a2c3caf19f
qenu <xchedeiklo@gmail.com>
2021-08-11 23:18:50 +0800

    update

--- qenutils/qenutils.py
+++ qenutils/qenutils.py
@@ -1,4 +1,6 @@
 import os
+import socket
+import time
 from typing import Literal, Optional
 
 import discord
@@ -25,3 +27,40 @@ class Qenutils(commands.Cog):
 
     async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int) -> None:
         super().red_delete_data_for_user(requester=requester, user_id=user_id)
+
+    async def latency_point(self, host: str, port: str, timeout: float = 5, offset: bool = False) -> Optional[float]:
+        '''
+            full credit to : https://github.com/dgzlopes/tcp-latency
+        '''
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        s_start = time()
+
+        try:
+            s.connect((host, int(port)))
+            s.shutdown(socket.SHUT_RD)
+
+        except socket.timeout:
+            return None
+        except OSError:
+            return None
+
+        s_runtime = (time() - s_start) * 1000
+
+        return round(float(s_runtime)-130, 2) if offset is False else round(float(s_runtime), 2)
+
+
+    @commands.command(name='tcping')
+    @commands.admin_or_permissions(manage_server=True)
+    async def tcping(self, ctx: commands.Context, host: str, port: int=443):
+        '''
+            Pings a server with port with bot
+            [p]tcping <host> [port]
+            Default port: 443
+        '''
+        latency = await self.latency_point(host=host, port=port, offset=True)
+        await ctx.tick()
+        if latency is None:
+            await ctx.send(f'{host} connection timed out!')
+            return
+        await ctx.send(f'{host} responded with {latency:.2f}ms latency.')
