9f780a8029fe41462c0f5469d952769e3c49bc13
qenu <xchedeiklo@gmail.com>
2021-12-10 17:30:19 +0800

    update

--- workflow/workflow.py
+++ workflow/workflow.py
@@ -330,7 +330,7 @@ class Workflow(commands.Cog):
         embed.title = "工作排程 Workflow"
         guild_data = await self.config.guild(ctx.guild).all()
         embed.description = (
-            f"工作排程文字頻道: {ctx.guild.get_channel(guild_data['channel_id']).mention}\n"
+            f"工作排程文字頻道: {ctx.guild.get_channel(guild_data['channel_id'])}\n"
             f"最後更新時間: <t:{guild_data['last_update']}:R>\n"
             "---\n"
             "**__委託__**\n"
