a25a6845b4ab30446d44b0441adda0f2ceb3f769
qenu <xchedeiklo@gmail.com>
2021-12-13 11:18:23 +0800

    update

--- workflow/workflow.py
+++ workflow/workflow.py
@@ -331,7 +331,8 @@ class Workflow(commands.Cog):
         quote_id : int
             The quotation id to update
         """
-        quote_data: dict = await self.config.guild(ctx.guild).quotations().get(quote_id)
+        quotes_data: dict = await self.config.guild(ctx.guild).quotations()
+        quote_data: dict = quotes_data.get(quote_id)
         quote: Quote = Quote(**quote_data)
         channel_id: int = await self.config.guild(ctx.guild).channel()
         if not channel_id:
